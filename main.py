43f90363a062211a84ea14b7fac48a21936e9e5a
J4nis05 <janis.roelli@gmail.com>
2024-09-10 14:45:42 +0200

    Added Support for .env Files

--- main.py
+++ main.py
@@ -1,23 +1,85 @@
+import pypresence
 from pypresence import Presence
 import time
 
+
+def load_env(file_path):
+    env_dict = {}
+
+    with open(file_path, "r") as file:
+        for line in file:
+            line = line.strip()
+
+            if not line or line.startswith("#"):
+                continue
+
+            key, value = line.split("=", 1)
+            value = value.strip('\'"')
+            env_dict[key.strip()] = value
+
+    return env_dict
+
+
+def set_value(value, value_type):
+    if value is None or 0 or '' and value_type is not bool:
+        return None
+    elif value is None or 0 or '' and value_type is bool:
+        return True
+
+    match value_type:
+        case 'int':
+            return int(value)
+        case 'str':
+            return str(value)
+        case 'bool':
+            return bool(value)
+        case _:
+            print("Unknown value type")
+            return value
+
+
+def set_party_size(party_min, party_max):
+    return [set_value(party_min, 'int'), set_value(party_max, 'int')]
+
+
+def set_buttons(b1_label, b1_url, b2_label, b2_url):
+    # Check Weather one or both buttons contain an empty element
+    if b1_label is None or 0 or '' or b1_url is None or 0 or '':
+        if b2_label is None or 0 or '' or b2_url is None or 0 or '':
+            # Buttons 1 & 2 Contain Empty Elements
+            return None
+        else:
+            # Button 1 Contains empty Elements, Return Stuff from Button 2
+            element = [{'label': b2_label, 'url': b2_url}]
+    else:
+        if b2_label is None or 0 or '' or b2_url is None or 0 or '':
+            # Button 1 Has Stuff, but Button 2 is Empty, Return Button 1
+            element = [{'label': b1_label, 'url': b1_url}]
+        else:
+            # Return Both Buttons
+            element = [{'label': b1_label, 'url': b1_url}, {'label': b2_label, 'url': b2_url}]
+
+    return element
+
+
 def update_presence():
     RPC.update(
-        state='Playing Solo',
-        details='Competitive',
-        start=1108663200,
-        # end=1739815200,
-        large_image='autism',
-        large_text='UwU',
-        small_image='frog',
-        small_text='Frog - Level 19',
-        party_id='429525273965363202',
-        party_size=[1, 2],
-        buttons=[
-            {'label': 'GitHub', 'url': 'https://github.com/J4nis05'},
-#            {'label': 'Website', 'url': 'https://www.J4nis05.ch'},
-            {'label': 'YouTube', 'url': 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'}
-        ]
+        state=set_value(env['drp_state'], str),
+        details=set_value(env['drp_details'], str),
+        start=set_value(env['drp_start'], int),
+        end=set_value(env['drp_end'], int),
+        large_image=set_value(env['drp_large_image'], str),
+        large_text=set_value(env['drp_large_text'], str),
+        small_image=set_value(env['drp_small_image'], str),
+        small_text=set_value(env['drp_small_text'], str),
+        party_id=set_value(env['drp_party_id'], str),
+        party_size=set_party_size(env['drp_party_min'], env['drp_party_max']),
+        join=set_value(env['drp_join_hash'], str),
+        spectate=set_value(env['drp_spectate_hash'], str),
+        match=set_value(env['drp_match_hash'], str),
+        buttons=set_buttons(env['drp_button1_label'], env['drp_button1_url'],
+                            env['drp_button2_label'], env['drp_button2_url']),
+        instance=set_value(env['drp_instance'], bool)
     )
 
 
@@ -36,16 +98,18 @@ def main():
                 return
 
 
-app_id = '123456'
-RPC = Presence(app_id)
-print('Setting Application ID... ')
+env = load_env('sample.env')
 
+RPC = Presence(env['app_id'])
+print('Setting Application ID ... ')
 RPC.connect()
-print('Connecting To Discord... ')
-
-print('Setting Discord Rich Presence... ')
+print('Connecting To Discord ... ')
+print('Setting Discord Rich Presence ... ')
 update_presence()
 print('Discord Rich Presence Updated Successfully!')
 
+
 while True:
     main()
+
+
